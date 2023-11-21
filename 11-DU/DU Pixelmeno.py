342089814fce679a82dccc940c709c473c70309b
Peťo <petermacus134@gmail.com>
2023-11-21 20:44:35 +0100

    vec

---
+++ 11-DU/DU Pixelmeno.py
@@ -0,0 +1,16 @@
+import tkinter
+import random
+canvas = tkinter.Canvas(width=1000,height=600)
+canvas.pack()
+
+color = random.choice("green","yellow","purple","blue","pink","black","red","orange","grey")
+
+x = 10
+y = 10
+d = 20
+
+#robime elke pisamenko P
+canvas.
+
+
+canvas.mainloop()
